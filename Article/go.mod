7ad968d563877c81d1db06e2adcdd17274eade6f
Abdurashid <abdurashidgaybullayev@gmail.com>
2022-02-21 23:02:15 +0500

    swag init

--- Article/go.mod
+++ Article/go.mod
@@ -2,4 +2,8 @@ module gitlab.com/Udevs/Article
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.7.7
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/swaggo/gin-swagger v1.4.1
+	github.com/swaggo/swag v1.7.9
+)
--- Article/server/server.go
+++ Article/server/server.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
 
+	_ "gitlab.com/Udevs/Article/server/docs"
 	"github.com/gin-gonic/gin"
 	"gitlab.com/Udevs/Article/models"
 	"gitlab.com/Udevs/Article/storage"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 var articleStorage storage.ArticleStorage
 
+// @title Swager Example API
+// @version 1.1
+// @description this is a sample article demo
+// @termsOfService https://udevs.io
+// @host localhost:8080
 func main() {
 	articleStorage = storage.NewArticleStorage()
 
@@ -20,13 +27,29 @@ func main() {
 	router.GET("/articles", getArticle)
 	router.GET("/articles/id",getArticleById)
 
+	router.GET("/swagger/*any",ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.Run(":8080")
 }
 
+type DefaultError struct {
+	Message string `json:"message"`
+}
+
+// @Router /articles [POST]
+// @Summary Create articels
+// @Description API for creating articles
+// @Tags article
+// @Accept json
+// @Produce json
+// @Param article body models.Article true "normative"
+// @Success 201 {object} string "ok"
+// @Failure 400,404 {object} DefaultError
+// @Failure default {object} DefaultError
+// @Failure 503 {object} DefaultError
 func createArticle(c *gin.Context) {
 	var article models.Article
-	t := time.Now()
-	article.CreatedAt = &t
+	// t := time.Now()
+	// article.CreatedAt = &t
 
 	if err := c.BindJSON(&article); err != nil {
 		log.Println(err)
@@ -43,6 +66,16 @@ func createArticle(c *gin.Context) {
 	c.JSON(201, "Succes")
 }
 
+// GetAllHandler godoc
+// @Tags article
+// @Summary Show All Article
+// @Description it gets all artciles from memory
+// @ID get-all-handler
+// @Accept json
+// @Produce json
+// @Success 200 {array} models.Article
+// @Failure default {object} DefaultError
+// @Router /articles [get]
 func getArticle(c *gin.Context) {
 	resp := articleStorage.GetList()
 	if resp == nil {
