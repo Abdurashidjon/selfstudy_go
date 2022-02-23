0f961a2495b6470d67f2cfed6b8c1904c1260da9
Abdurashid <abdurashidgaybullayev@gmail.com>
2022-02-23 16:23:00 +0500

    Change

---
+++ database/go.mod
@@ -0,0 +1,5 @@
+module gitlab.com/Udevs/database
+
+go 1.14
+
+require github.com/jmoiron/sqlx v1.3.4
---
+++ database/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func main()  {
+	var  db *sqlx.DB
+	fmt.Println(db)
+}
